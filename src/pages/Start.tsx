3923f86fec9733ca409d05d626b8bc64e1b35dac
bvv-1 <fhinnoigsr@gmail.com>
2024-06-08 18:54:56 +0900

    Update Start.tsx

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,34 +1,29 @@
-import { Button, TextField, styled } from "@mui/material"
+import { Button, TextField, styled, Grid } from "@mui/material"
 import logo from "../assets/bowling_logo.png"
 
 interface Props {
   setUiState: (uiState: string) => void
 }
 
-function Start(props: Props) {
+export default function Start(props: Props) {
   const RedButton = styled(Button)({
     backgroundColor: "#D25E5E",
     "&:hover": {
       backgroundColor: "darkred",
     },
     color: "white",
-    width: "100%",
+    width: "30%",
   })
   return (
-    <>
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+    >
       <img src={logo} alt="壁よけボウリング" width={"50%"} />
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <TextField id="outlined-basic" label="名前" variant="outlined" />
-        <RedButton onClick={() => props.setUiState("Play")}>プレイ</RedButton>
-      </div>
-    </>
+      <TextField id="outlined-basic" label="ユーザー名" variant="outlined" margin="normal" style={{ width: "30%" }} />
+      <RedButton onClick={() => props.setUiState("Play")}>プレイ</RedButton>
+    </Grid>
   )
 }
-
-export default Start
