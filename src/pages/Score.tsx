b0097699c42861ad76ef9245ee161a9e55977394
bvv-1 <fhinnoigsr@gmail.com>
2024-06-08 15:32:39 +0900

    format

--- src/pages/Score.tsx
+++ src/pages/Score.tsx
@@ -1,18 +1,21 @@
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography } from "@mui/material";
-import { TypeScore } from "../App";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography } from "@mui/material"
+import { TypeScore } from "../App"
 
 interface Props {
-  scores: TypeScore[];
+  scores: TypeScore[]
 }
 
-const currentScore = 100;
+const currentScore = 100
 
 export default function Score(props: Props) {
-  const scores = props.scores.length > 0 ? props.scores : [
-    { stage: 1, score: 150 },
-    { stage: 2, score: 200 },
-    { stage: 3, score: 180 },
-  ];
+  const scores =
+    props.scores.length > 0
+      ? props.scores
+      : [
+          { stage: 1, score: 150 },
+          { stage: 2, score: 200 },
+          { stage: 3, score: 180 },
+        ]
 
   return (
     <div>
@@ -37,5 +40,5 @@ export default function Score(props: Props) {
         </Table>
       </TableContainer>
     </div>
-  );
+  )
 }
