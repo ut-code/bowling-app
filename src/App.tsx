768159baab022ee85d4af22f62f443d43af66427
bvv-1 <fhinnoigsr@gmail.com>
2024-06-08 12:33:35 +0900

    Add uistate

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,17 @@
-import Matter from "matter-js";
-import { useEffect } from "react";
+import { useState } from "react";
+import Example from "./pages/Example";
 
 function App() {
-  useEffect(() => {
-    // エンジンの作成
-    const engine = Matter.Engine.create();
-
-    // レンダラーの作成
-    const render = Matter.Render.create({
-      element: document.body,
-      engine: engine,
-      options: {
-        width: 800,
-        height: 600,
-        wireframes: false,
-      },
-    });
-
-    // 固定された地面を作成
-    const ground = Matter.Bodies.rectangle(400, 550, 800, 60, { isStatic: true });
-
-    // ボールを作成
-    const ball = Matter.Bodies.circle(400, 100, 50, {
-      density: 0.04,
-      frictionAir: 0.005,
-      restitution: 0.8,
-      friction: 0.01,
-    });
-
-    // 世界にボディを追加
-    Matter.World.add(engine.world, [ball, ground]);
-
-    // エンジンとレンダラーの実行
-    Matter.Engine.run(engine);
-    Matter.Render.run(render);
-
-    // コンポーネントのアンマウント時にレンダラーとエンジンを停止
-    return () => {
-      Matter.Render.stop(render);
-      Matter.Engine.clear(engine);
-      render.canvas.remove();
-    };
-  }, []);
-
+  const [uiState, setUiState] = useState("Start");
+  
   return (
     <div>
-      <h1>Matter.js with React</h1>
+      {uiState === "Start" && <p>start</p>}
+      {uiState === "Score" && <p>score</p>}
+      {uiState === "Example" && <Example />}
+      <button onClick={() => setUiState("Example")}>Example</button>
+      <button onClick={() => setUiState("Start")}>Start</button>
+      <button onClick={() => setUiState("Score")}>Score</button>
     </div>
   );
 }
